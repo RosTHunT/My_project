77dfaef7b0b925680316a881be58d22461a9127d
RosTHunT <rostyslav1995.95@gmail.com>
2022-06-22 18:48:58 +0300

    update/update front/shop

--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
+from cart.cart import Cart
 from cart.forms import CartAddProductForm
 from .models import Product, Category
 
 
 def home_page(request):
     categories = Category.objects.all()
+    cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'],
+                                                                   'update': True})
     context = {
         'categories': categories,
+        'cart': cart,
     }
     return render(request, template_name='shop/home_page.html', context=context)
 
@@ -16,11 +22,16 @@ def get_category(request, category_id):
     product = Product.objects.filter(category_id=category_id)
     category = get_object_or_404(Category, id=category_id)
     cart_product_form = CartAddProductForm()
+    cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'],
+                                                                   'update': True})
 
     context ={
         'category': category,
         'product': product,
         'cart_product_form': cart_product_form,
+        'cart': cart,
     }
     return render(request, 'shop/category.html', context=context)
 
